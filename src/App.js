9ea385614172935db08770b8c043c7e91093b11f
ericjchung <ericjchung@protonmail.com>
2019-08-11 11:26:42 +0000

    Added copyrights

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import PokemonState from "./context/pokemon/PokemonState";
 import Entry from './components/pages/Entry'
 import Navbar from './components/layout/Navbar'
-
+import Copyright from "./components/layout/Copyright"
 function App() {
   return (
     <PokemonState>
       <div className="App bg-primary">
         <Navbar />
         <Entry />
+        <Copyright />
       </div>
     </PokemonState>
   );
---
+++ src/components/layout/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Copyright = () => {
+    return (
+        <div className="container bg-primary">
+            <h1>Pokémon. © 1995-2019</h1>
+        </div>
+    )
+}
+
+export default Copyright

96945e70ee814fda969567e43f438694b5c4be78
ericjchung <ericjchung@protonmail.com>
2019-08-12 02:23:37 +0000

    Branch Test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Entry from './components/pages/Entry'
 import Navbar from './components/layout/Navbar'
 import Copyright from "./components/layout/Copyright"
 function App() {
+  //branching
   return (
     <PokemonState>
       <div className="App bg-primary">
